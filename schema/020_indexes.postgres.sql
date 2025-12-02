f7e8f8c70af2c4ec1c72bb21523d20b0634de3b7
Vito416 <vito416@protonmail.com>
2025-12-02 19:28:46 +0100

    Sync submodule state after YAML views + upsert/coverage fixes

--- schema/020_indexes.postgres.sql
+++ schema/020_indexes.postgres.sql
@@ -1,5 +1,7 @@
--- Auto-generated from schema-map-postgres.psd1 (map@mtime:2025-11-21T00:25:46Z)
+-- Auto-generated from schema-map-postgres.yaml (map@94ebe6c)
 -- engine: postgres
 -- table:  tenants
 
 CREATE UNIQUE INDEX IF NOT EXISTS ux_tenants_slug_live_ci ON tenants (slug_ci) WHERE deleted_at IS NULL;
+
+CREATE UNIQUE INDEX IF NOT EXISTS ux_tenants_slug_ci_is_live ON tenants (slug_ci, is_live);
